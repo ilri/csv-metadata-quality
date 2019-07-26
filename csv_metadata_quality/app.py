18f26c343ddbcd528c4d5d636e1052ff3b373610
Alan Orth <alan.orth@gmail.com>
2019-07-27 00:25:30 +0300

    csv_metadata_quality/app.py: Fix path to test.csv

--- csv_metadata_quality/app.py
+++ csv_metadata_quality/app.py
@@ -6,7 +6,7 @@ def run():
     # Read all fields as strings so dates don't get converted from 1998 to 1998.0
     #df = pd.read_csv('/home/aorth/Downloads/2019-07-26-Bioversity-Migration.csv', dtype=str)
     #df = pd.read_csv('/tmp/quality.csv', dtype=str)
-    df = pd.read_csv('tests/test.csv', dtype=str)
+    df = pd.read_csv('data/test.csv', dtype=str)
 
     # Fix whitespace in all columns
     for column in df.columns.values.tolist():
