41a30f1b07d94d01314db5ba063316855c2047a5
Alan Orth <alan.orth@gmail.com>
2019-07-27 00:36:40 +0300

    Add initial tests
    For now only test fixes because they return changed data. I'm not
    sure how to test the checks, because they don't return data and I
    can't modify them to return boolean values without breaking the app.

---
+++ tests/test_fix.py
@@ -0,0 +1,23 @@
+import csv_metadata_quality.fix as fix
+import pytest
+
+def test_fix_leading_whitespace():
+    '''Test fixing leading whitespace.'''
+
+    value = ' Alan'
+
+    assert fix.whitespace(value) == 'Alan'
+
+def test_fix_trailing_whitespace():
+    '''Test fixing trailing whitespace.'''
+
+    value = 'Alan '
+
+    assert fix.whitespace(value) == 'Alan'
+
+def test_fix_excessive_whitespace():
+    '''Test fixing excessive whitespace.'''
+
+    value = 'Alan  Orth'
+
+    assert fix.whitespace(value) == 'Alan Orth'
