dfd961d720f4a9ba4e7ccc5bb4321b4abe6ffba8
Alan Orth <alan.orth@gmail.com>
2019-07-26 23:14:37 +0300

    Bring test.csv into project

--- csv_metadata_quality/app.py
+++ csv_metadata_quality/app.py
@@ -6,7 +6,7 @@ def run():
     # Read all fields as strings so dates don't get converted from 1998 to 1998.0
     #df = pd.read_csv('/home/aorth/Downloads/2019-07-26-Bioversity-Migration.csv', dtype=str)
     #df = pd.read_csv('/tmp/quality.csv', dtype=str)
-    df = pd.read_csv('/tmp/omg.csv', dtype=str)
+    df = pd.read_csv('tests/test.csv', dtype=str)
 
     # Fix whitespace in all columns
     for column in df.columns.values.tolist():
@@ -21,4 +21,4 @@ def run():
             df[column] = df[column].apply(check.isbn)
 
     # Write
-    df.to_csv('/tmp/omg.fixed.csv', index=False)
+    df.to_csv('/tmp/test.fixed.csv', index=False)
