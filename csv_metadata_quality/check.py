2a1566af623d3144fa87e32ce525552649b7147e
Alan Orth <alan.orth@gmail.com>
2020-07-06 13:44:46 +0300

    csv_metadata_quality/check.py: Parameterize AGROVOC request

--- csv_metadata_quality/check.py
+++ csv_metadata_quality/check.py
@@ -232,11 +232,10 @@ def agrovoc(field, field_name):
 
     # Try to split multi-value field on "||" separator
     for value in field.split("||"):
-        request_url = (
-            f"http://agrovoc.uniroma2.it/agrovoc/rest/v1/agrovoc/search?query={value}"
-        )
+        request_url = "http://agrovoc.uniroma2.it/agrovoc/rest/v1/agrovoc/search"
+        request_params = {"query": value}
 
-        request = requests.get(request_url)
+        request = requests.get(request_url, params=request_params)
 
         if request.status_code == requests.codes.ok:
             data = request.json()
