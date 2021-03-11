fa84cfa440743ed40295f8c3cd98a27abe396ef2
Alan Orth <alan.orth@gmail.com>
2021-03-11 22:44:36 +0200

    Bump version to 0.4.6-dev

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 setuptools.setup(
     name="csv-metadata-quality",
-    version="0.4.6",
+    version="0.4.6-dev",
     author="Alan Orth",
     author_email="aorth@mjanja.ch",
     description="A simple, but opinionated CSV quality checking and fixing pipeline for CSVs in the DSpace ecosystem.",
