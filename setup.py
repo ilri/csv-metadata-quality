be609a809da18c1d9f8ba256f43b0430b7ad358e
Alan Orth <alan.orth@gmail.com>
2023-05-29 21:32:59 +0300

    setup.py: add Python 3.11 classifier

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
